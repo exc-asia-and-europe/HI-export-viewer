c1ee9eb53b114fdbb93ed03d65405054b56775f2
ChiSamurai <guth@asia-europe.uni-heidelberg.de>
2016-10-20 11:06:03 +0200

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Task configuration.
+    jshint: {
+      options: {
+        curly: true,
+        debug: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: false,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        jquery: true,
+        globals: {
+          "svgPanZoom": false,
+          "d3": false,
+          "langCodeMap": false
+        }
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib_test: {
+        src: ['lib/main.js']
+//        src: ['lib/**/*.js', 'test/**/*.js']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint']);
+
+};
